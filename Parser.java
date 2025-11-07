c1c6c3946600a0dd61ff27928863b52a7b6c526a
Micheal Olawale <2437632@swansea.ac.uk>
2025-11-07 17:33:38 +0000

    Refactor example grid documentation and improve output formatting in Parser class

--- Parser.java
+++ Parser.java
@@ -4,27 +4,29 @@ public class Parser {
      * Build the example 2D ladder grid using LadderElements types.
      *
      * Grid layout (2 rows x 6 columns):
-     * [ left power rail , contact "x" , horizontal link , negated contact "y" , vertical link, coil "r" ],
-     * [ left power rail, horizontal link , contact "z" , horizontal link , End of Vertical Link, empty ]
+     * [ left power rail , contact "x" , horizontal link , negated contact "y" ,
+     * vertical link, coil "r" ],
+     * [ left power rail, horizontal link , contact "z" , horizontal link , End of
+     * Vertical Link, empty ]
      */
     public static LadderElements.LadderElement[][] buildExampleGrid() {
-        return new LadderElements.LadderElement[][]{
-            {
-                LadderElements.LeftPowerRail.INSTANCE,
-                new LadderElements.Contact("x"),
-                LadderElements.HorizontalLink.INSTANCE,
-                new LadderElements.NegatedContact("y"),
-                LadderElements.VerticalLink.INSTANCE,
-                new LadderElements.Coil("r")
-            },
-            {
-                LadderElements.LeftPowerRail.INSTANCE,
-                LadderElements.HorizontalLink.INSTANCE,
-                new LadderElements.Contact("z"),
-                LadderElements.HorizontalLink.INSTANCE,
-                LadderElements.EndOfVerticalLink.INSTANCE,
-                LadderElements.Empty.INSTANCE
-            }
+        return new LadderElements.LadderElement[][] {
+                {
+                        LadderElements.LeftPowerRail.INSTANCE,
+                        new LadderElements.Contact("x"),
+                        LadderElements.HorizontalLink.INSTANCE,
+                        new LadderElements.NegatedContact("y"),
+                        LadderElements.VerticalLink.INSTANCE,
+                        new LadderElements.Coil("r")
+                },
+                {
+                        LadderElements.LeftPowerRail.INSTANCE,
+                        LadderElements.HorizontalLink.INSTANCE,
+                        new LadderElements.Contact("z"),
+                        LadderElements.HorizontalLink.INSTANCE,
+                        LadderElements.EndOfVerticalLink.INSTANCE,
+                        LadderElements.Empty.INSTANCE
+                }
         };
     }
 
@@ -34,12 +36,13 @@ public class Parser {
 
         System.out.println("Example ladder grid:");
         for (int r = 0; r < grid.length; r++) {
-            System.out.print("Row " + r + ": [ ");
-            for (int c = 0; c < grid[r].length; c++) {
+            System.out.print("Row " + r + ": ");
+            for (int c = grid[r].length - 1; c >= 0; c--) {
                 System.out.print(grid[r][c]);
-                if (c < grid[r].length - 1) System.out.print(" , ");
+                if (c > 0)
+                    System.out.print(" , ");
             }
-            System.out.println(" ]");
+            System.out.println();
         }
     }
 
